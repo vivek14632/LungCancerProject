14ed58fb7c56b69a6c0a7b70ad352da9b789d872
Vivek Kumar Singh <vksingh.iiitb@gmail.com>
2017-03-25 00:42:34 -0400

    Update imageProcessing.py

--- imageProcessing.py
+++ imageProcessing.py
@@ -18,10 +18,10 @@ def load_scan(path):
     slices.sort(key = lambda x: float(x.ImagePositionPatient[2]))
     try:
         slice_thickness = np.abs(slices[0].ImagePositionPatient[2] - slices[1].ImagePositionPatient[2])
-         print(slice_thickness)
+        print(slice_thickness)
     except:
         slice_thickness = np.abs(slices[0].SliceLocation - slices[1].SliceLocation)
-         print(slice_thickness)
+        print(slice_thickness)
         
     for s in slices:
         s.SliceThickness = slice_thickness
