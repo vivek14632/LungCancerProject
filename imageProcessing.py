1770a20bcc6877f8e75e03e9b757a480180a71b1
Vivek Kumar Singh <vksingh.iiitb@gmail.com>
2017-04-09 12:00:23 -0400

    Update imageProcessing.py

--- imageProcessing.py
+++ imageProcessing.py
@@ -1,3 +1,6 @@
+#Requirements before running this file
+#sudo apt-get install python-tk
+
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import dicom
