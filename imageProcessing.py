60d3ee4b9a3563f1f1174d15b43ccebc5780e036
Vivek Kumar Singh <vksingh.iiitb@gmail.com>
2017-04-09 12:22:53 -0400

    Update imageProcessing.py

--- imageProcessing.py
+++ imageProcessing.py
@@ -58,7 +58,9 @@ def get_pixels_hu(slices):
 
 first_patient = load_scan(INPUT_FOLDER + patients[0])
 first_patient_pixels = get_pixels_hu(first_patient)
-numpy.ndarray.tofile('/home/ravi/image.txt',"",%s)
+fp1=open('/home/ravi/file.txt','w')
+numpy.ndarray.tofile(fp1,"",%s)
+fp.close()
 plt.hist(first_patient_pixels.flatten(), bins=80, color='c')
 plt.xlabel("Hounsfield Units (HU)")
 plt.ylabel("Frequency")
