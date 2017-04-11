5fbc125ecba411fc0e96627b7d84a69ea06ed78f
Vivek Kumar Singh <vksingh.iiitb@gmail.com>
2017-04-11 15:55:27 -0400

    Update script.R

--- pixelDistribution/script.R
+++ pixelDistribution/script.R
@@ -1,3 +1,3 @@
 NumOfUniquePixels=4000
-NumberofPatiensts=20
-f_mat=matrix(rep 0, 4000, NumOfUniquePixels*NumberofPatiensts)
+NumberofImages=20
+f_mat=matrix(rep 0, 4000, NumOfUniquePixels*NumberofImages)
