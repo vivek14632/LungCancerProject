44259965401660ae234ea4df594256c31eb42ea4
nargis01 <nargis01@gmail.com>
2017-05-05 18:00:11 +0530

    Update script.R

--- pixelDistribution/script.R
+++ pixelDistribution/script.R
@@ -1,3 +1,7 @@
 NumOfUniquePixels=4000
 NumberofImages=20
 f_mat=matrix(rep 0, 4000, NumOfUniquePixels*NumberofImages)
+
+
+# https://www.raddq.com/dicom-processing-segmentation-visualization-in-python/ 
+# read the above link for understanding pixel distribution
