5b0a7016dc80a3acd9a2bf6a9322b1b2af4ed750
Vivek Kumar Singh <vksingh.iiitb@gmail.com>
2017-05-21 07:52:41 -0400

    Update countDicom.py

--- vivek/countDicom.py
+++ vivek/countDicom.py
@@ -1,6 +1,14 @@
+# This file is used to count number of DICOM
+# files in any folder consisting of patients' folders
+# such as stage1 or stage2 or sample_images
+
+# the following code is useful to get the username to identify
+# the computer on which the code is running and use the corresponding
+# path where we have stored the data
 if(getpass.getuser()=='vivek4'):
 	dirname='/work/v/vivek4/stage1'
 else:
+	# please provide your path to data directory	
 	print('error: specify directory name for the stage 1')
 
 #total number of dicom files
