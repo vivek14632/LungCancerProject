4886d42968070fdb4c81cf78039193010f056bdb
Vivek Kumar Singh <vksingh.iiitb@gmail.com>
2017-05-21 16:13:45 -0400

    Update and rename preprocessing2_vivek.py to preprocessing.py

--- vivek/preprocessing.py
+++ vivek/preprocessing.py
@@ -123,21 +123,3 @@ def get_segmented_lungs(im, plot=False):
 def segment_lung_from_ct_scan(ct_scan):
     return np.asarray([get_segmented_lungs(slice) for slice in ct_scan])
 
-if(getpass.getuser()=='vivek4'):
-	dirname='/work/v/vivek4/stage1'
-else:
-	print('error: specify directory name for the stage 1')
-
-#to find min and max in different folders
-for directories in os.listdir(dirname):
-
-	ct_scan = read_ct_scan(dirname+'/'+directories+'/')
-	segmented_ct_scan = segment_lung_from_ct_scan(ct_scan)
-	segmented_ct_scan[segmented_ct_scan < 604] = 0 # not sure if we should do this. I got everything black after I did this transformation
-
-	print('folder name='+directories)
-	print('min='+str(np.amin(segmented_ct_scan)))
-	print('max='+str(np.amax(segmented_ct_scan)))
-
-
-
