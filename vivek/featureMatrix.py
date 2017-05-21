16513601cb1c28047b108178e631d0434d6a4699
vivek14632 <vksingh.iiitb@gmail.com>
2017-05-21 16:41:55 -0400

    changes

---
+++ vivek/featureMatrix.py
@@ -0,0 +1,16 @@
+# author: vivek
+# Purpose: To create feature matrix for modeling
+
+# the number of rows in the feature matrix is 
+# number of dicom images
+
+# the number of columns is 0-4095. These represents pixel values
+
+import  preprocessing
+
+PATH_TO_IMAGES='/work/v/vivek4/sample_images'
+
+# First we need to find the number of dicom images in any folder
+
+
+
