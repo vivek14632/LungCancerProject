e6776331e7c91b4d3cb0f270ac293c5cea1684e1
vivek14632 <vksingh.iiitb@gmail.com>
2017-05-21 17:08:41 -0400

    working with numpy

--- vivek/NumpyMatrix.py
+++ vivek/NumpyMatrix.py
@@ -1,6 +1,6 @@
 # author: vivek
 
-# this code is used to store the numpy matrix
+# this code is used to store and read the numpy matrix
 
 #create a numpy matrix
 
@@ -10,12 +10,14 @@ def saveNumpy(mObject, mFileName):
 	image=np.zeros((1000,10))
 	np.save(mFileName,mObject)
 
+def readNumpy(mFileName):
+	return np.load(mFileName)
 
 #test code
 if __name__ == '__main__':
 	image=np.zeros((1000,10))
 	saveNumpy(image,'hello')
 
-
+	print(np.shape(readNumpy('hello.npy')))
 
 
