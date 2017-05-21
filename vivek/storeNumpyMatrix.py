afe467c8ebd890b008dc7234c69bb4678b555566
vivek14632 <vksingh.iiitb@gmail.com>
2017-05-21 17:01:26 -0400

    store a numpy

--- vivek/storeNumpyMatrix.py
+++ vivek/storeNumpyMatrix.py
@@ -2,3 +2,20 @@
 
 # this code is used to store the numpy matrix
 
+#create a numpy matrix
+
+import numpy as np
+
+def saveNumpy(mObject, mFileName):
+	image=np.zeros((1000,10))
+	np.save(mFileName,mObject)
+
+
+#test code
+if __name__ == '__main__':
+	image=np.zeros((1000,10))
+	saveNumpy(image,'hello')
+
+
+
+
