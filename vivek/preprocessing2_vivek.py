2c1c4bb8fd456eff816c541fe90ea8865debac58
vivek14632 <vivek4@itn3.rc.usf.edu>
2017-05-20 22:26:52 -0400

    changes

--- vivek/preprocessing2_vivek.py
+++ vivek/preprocessing2_vivek.py
@@ -18,6 +18,8 @@ import scipy.misc
 import numpy as np
 import matplotlib
 
+import getpass
+
 def read_ct_scan(folder_name):
         # Read the slices from the dicom file
         slices = [dicom.read_file(folder_name + filename) for filename in os.listdir(folder_name)]
@@ -121,10 +123,20 @@ def get_segmented_lungs(im, plot=False):
 def segment_lung_from_ct_scan(ct_scan):
     return np.asarray([get_segmented_lungs(slice) for slice in ct_scan])
 
-ct_scan = read_ct_scan('C:/Users/ThinkPad/Documents/00cba091fa4ad62cc3200a657aeb957e/') # array of pixel_array
-segmented_ct_scan = segment_lung_from_ct_scan(ct_scan)
-segmented_ct_scan[segmented_ct_scan < 604] = 0 # not sure if we should do this. I got everything black after I did this transformation
+if(getpass.getuser()=='vivek4'):
+	dirname='/work/v/vivek4/stage1'
+else:
+	print('error: specify directory name for the stage 1')
 
-minimumPixels=np.array([],dtype=np.int16)
-maximumPixels=np.array([],dtype=np.int16)
+for directories in os.listdir(dirname):
+
+	ct_scan = read_ct_scan(dirname+'/'+directories+'/')
+	segmented_ct_scan = segment_lung_from_ct_scan(ct_scan)
+	segmented_ct_scan[segmented_ct_scan < 604] = 0 # not sure if we should do this. I got everything black after I did this transformation
+
+	print('folder name='+directories)
+	print('min='+str(np.amin(segmented_ct_scan)))
+	print('max='+str(np.amax(segmented_ct_scan)))
+
+
 
