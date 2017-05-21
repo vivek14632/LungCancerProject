f6f0c97b560cc0209f0f5fff4b304699a37a2697
vivek14632 <vksingh.iiitb@gmail.com>
2017-05-21 16:50:47 -0400

    count dicom complete

--- vivek/countDicom.py
+++ vivek/countDicom.py
@@ -7,22 +7,17 @@
 # path where we have stored the data
 
 import os
-import getpass
 
-if(getpass.getuser()=='vivek4'):
-	#dirname='/work/v/vivek4/stage1'
-	dirname='/work/v/vivek4/sample_images'
-else:
-	# please provide your path to data directory	
-	print('error: specify directory name for the stage 1')
+def countDicomInaFolder(folderName):
+	#total number of dicom files
+	count=0
+	# we need to count number of dicom files to create numpy matrix
+	for directories in os.listdir(folderName):
+		files=os.listdir(folderName+'/'+directories+'/')
+		count=count+len(files)
+	print('Total number of dcm files='+str(count))
+	return count 
 
-#total number of dicom files
-count=0
-
-# we need to count number of dicom files to create numpy matrix
-for directories in os.listdir(dirname):
-	files=os.listdir(dirname+'/'+directories+'/')
-	count=count+len(files)
-
-print('Total number of dcm files='+str(count)) 
-
+# test code
+if __name__ == '__main__':
+	print(str(countDicomInaFolder('/work/v/vivek4/sample_images')))
