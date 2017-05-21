19765067620c2a0be6369331a05d3131daabb652
Vivek Kumar Singh <vksingh.iiitb@gmail.com>
2017-05-21 07:57:31 -0400

    Update submitCountDicom.sh

--- vivek/submitCountDicom.sh
+++ vivek/submitCountDicom.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
 #
+#SBATCH --comment=r-test
+#SBATCH --ntasks=1
+#SBATCH --job-name=CountDicom
+#SBATCH --output=output.%j.r-test
+#SBATCH --time=01:00:00
+
+#### SLURM 4 processor R test to run for 1 hour.
+
+module purge
+module add apps/python/3.5.1
+
+python countDicom.py
