56ae3a110427ec59f2dbb12ddbe81efa2252d4c8
ShankarMaruvada <ravishankarm@mail.usf.edu>
2017-05-26 12:57:57 -0400

    Update FinalFrequencyMatrix.py

--- pixelDistribution/FinalFrequencyMatrix.py
+++ pixelDistribution/FinalFrequencyMatrix.py
@@ -9,19 +9,18 @@ def print_final_matrix (data_dir):
     data_dir = '/home/cis1024/sample_images_clean/'
     patients = os.listdir(data_dir)
     
-#count total number of slices
+    #Lets keep track of row in the final matrix
+    finalMatrixRowCounter=0
+    #count total number of slices
     total_slices = 0
     for patient in patients[:]:
         mat = np.load(data_dir + patient)
         total_slices += len(mat)
-    	#define final freq matrix
     
+    #define final freq matrix
     final_matrix = np.zeros(total_slices*4096, dtype = int).reshape(total_slices,4096)
     #print final_matrix
-	
-    #Lets keep track of row in the final matrix
-    finalMatrixRowCounter=0
-	
+    
     for patient in patients[:]:
         mat_new = np.load(data_dir + patient)
         for each in range(len(mat_new)):
@@ -32,8 +31,7 @@ def print_final_matrix (data_dir):
             freq_mat= np.asarray((unique, counts)).T    
             #update final matrix numpy array
             for j in range(len(freq_mat)):
-                final_matrix[finalMatrixRowCounter][freq_mat[j][0]] = freq_mat[j][1]
-			finalMatrixRowCounter=finalMatrixRowCounter+1
-    #print final_matrix
+                final_matrix[each][freq_mat[j][0]] = freq_mat[j][1]
+                finalMatrixRowCounter=finalMatrixRowCounter+1
     #save matrix
     np.save(destination_dir + 'final_matrix',final_matrix)
