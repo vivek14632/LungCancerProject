4664699324670e395fcb3aa6f7eaf9b39f2bfb82
ShankarMaruvada <ravishankarm@mail.usf.edu>
2017-05-26 16:37:23 -0400

    Update GradientBoostingModel.py

--- models/GradientBoostingModel.py
+++ models/GradientBoostingModel.py
@@ -1,1 +1,17 @@
-import 
+import numpy as np
+from sklearn.datasets import make_hastie_10_2
+from sklearn.ensemble import GradientBoostingClassifier
+from data import *
+
+x_dir ="X_MAT_PATH"
+y_dir ="Y_MAT_PATH"
+
+X = np.load(x_dir)
+y = np.load(y_dir)
+
+
+X_train, X_test = X[:2000], X[2000:]
+y_train, y_test = y[:2000], y[2000:]
+
+clf = GradientBoostingClassifier(n_estimators=100, learning_rate=1.0,max_depth=1, random_state=0).fit(X_train, y_train)
+clf.score(X_test, y_test)
