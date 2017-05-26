7b627c782210343b13b702105084eb1aaf58dac4
ShankarMaruvada <ravishankarm@mail.usf.edu>
2017-05-26 16:39:12 -0400

    Create MLPModel.py

---
+++ models/MLPModel.py
@@ -0,0 +1,20 @@
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import confusion_matrix
+import numpy as np
+from data import *
+
+x_dir =X_MAT_PATH
+y_dir =Y_MAT_PATH
+
+X = np.load(x_dir)
+y = np.load(y_dir)
+
+clf = MLPClassifier(solver='lbfgs', alpha=0.5,hidden_layer_sizes=(5, 2), random_state=1)
+clf.fit(X[:2000], y[:2000])                         
+
+
+prediction = clf.predict(X[2000:3604])
+
+cm = confusion_matrix(y[2000:3604],prediction)
+
+#accuracy 64.4%
