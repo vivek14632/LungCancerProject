09543cb5c119c2cbfa58a42a21791fa2f86d1311
Rhomi <rhomielijah@gmail.com>
2017-05-26 16:42:32 -0400

    Update svmModel.py

--- models/svmModel.py
+++ models/svmModel.py
@@ -1,1 +1,37 @@
 # svm model
+import sklearn
+import numpy as np
+from sklearn import svm
+from sklearn.metrics import confusion_matrix
+
+from data import*
+
+X = np.load(X_MAT_PATH)
+y = np.load(Y_MAT_PATH)
+
+x_len = X.shape[0]
+print('Length of X is {}'.format(x_len))
+y_len = y.shape[0]
+print('Length of y is {}'.format(y_len))
+
+y.dtype
+# Converting dtype of y to int32 numpy array
+y1 = y.astype(int)
+y1.dtype
+print('dtype of y1 is {}'.format(y1.dtype))
+print('Number of 1s in y1 is {}'.format(sum(y1)))
+
+
+# Run the model  
+clf = svm.SVC()
+# Run the model on 80% data
+clf.fit(X[0:2727,], y1[0:2727])  
+# Predict on 20%
+pred = clf.predict(X[2728:X.shape[0]])
+
+cm = confusion_matrix(y1[2728:y1.shape[0]], pred)
+print('Confusion matrix \n'.format(cm))
+
+# Do a reverse confustion matrix
+cm = confusion_matrix(pred,y1[2728:y1.shape[0]])
+print('Reverse Confusion matrix \n'.format(cm))
