20553fb07a6691a90f9722c03aa18a4ba6b57e1e
Vivek Kumar Singh <vksingh.iiitb@gmail.com>
2017-05-29 13:19:16 -0400

    Update svmModel.py

--- models/svmModel.py
+++ models/svmModel.py
@@ -24,14 +24,20 @@ print('Number of 1s in y1 is {}'.format(sum(y1)))
 
 # Run the model  
 clf = svm.SVC()
+
 # Run the model on 80% data
+#@vivek: please remove the hard coded values
 clf.fit(X[0:2727,], y1[0:2727])  
+
 # Predict on 20%
+#@vivek: please remove the hard coded values
 pred = clf.predict(X[2728:X.shape[0]])
 
+#@vivek: please remove the hard coded values
 cm = confusion_matrix(y1[2728:y1.shape[0]], pred)
 print('Confusion matrix \n'.format(cm))
 
 # Do a reverse confustion matrix
+#@vivek: please remove the hard coded values
 cm = confusion_matrix(pred,y1[2728:y1.shape[0]])
 print('Reverse Confusion matrix \n'.format(cm))
