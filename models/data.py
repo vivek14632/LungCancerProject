93e32e595abe1a61573b77a8e888293537db9b96
Vivek Kumar Singh <vksingh.iiitb@gmail.com>
2017-05-29 14:01:48 -0400

    Update data.py

--- models/data.py
+++ models/data.py
@@ -1,2 +1,10 @@
-X_MAT_PATH='/home/ravi/final_matrix.npy'
-Y_MAT_PATH='/home/cis1024/sample_images_Y.npy'
+user= os.getlogin()
+if(user=='cis1024'):
+  X_MAT_PATH='/home/ravi/final_matrix.npy'
+  Y_MAT_PATH='/home/cis1024/sample_images_Y.npy'
+elif (user=='vivek4'):
+  X_MAT_PATH='/work/v/vivek4/final_matrix.npy'
+  Y_MAT_PATH='/work/v/vivek4/sample_images_Y.npy'
+  
+else:
+  print("Error: Please specifiy data path")
