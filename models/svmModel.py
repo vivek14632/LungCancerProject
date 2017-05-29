9367b783a747b107413a36a81d60bc9dd50a7f0d
Rhomi <rhomielijah@gmail.com>
2017-05-29 15:41:16 -0400

    Update svmModel.py
    Code changes for SVM Kernel Automation.

--- models/svmModel.py
+++ models/svmModel.py
@@ -1,43 +1,41 @@
-# svm model
-import sklearn
+import sklearn as sk
 import numpy as np
 from sklearn import svm
 from sklearn.metrics import confusion_matrix
 
 from data import*
+from train_test_split import *
 
 X = np.load(X_MAT_PATH)
-y = np.load(Y_MAT_PATH)
+Y = np.load(Y_MAT_PATH)
 
-x_len = X.shape[0]
-print('Length of X is {}'.format(x_len))
-y_len = y.shape[0]
-print('Length of y is {}'.format(y_len))
+print("X matrix with shape {} has been loaded".format(X.shape))
+print("Y matrix with shape {} has been loaded".format(Y.shape))
 
-y.dtype
-# Converting dtype of y to int32 numpy array
-y1 = y.astype(int)
-y1.dtype
-print('dtype of y1 is {}'.format(y1.dtype))
-print('Number of 1s in y1 is {}'.format(sum(y1)))
+# Converting dtype to int32
+X = X.astype(int)
+Y = Y.astype(int)
 
+# Creating training and testing sets
+train_x,test_x = get_train_test_data(X)
+print('Train and test data created for X matrix created with dimensions {} and {} respectively'.format(train_x.shape,test_x.shape))
 
-# Run the model  
-clf = svm.SVC()
+train_y,test_y = get_train_test_data(Y)
+print('Train and test data created for Y matrix created with dimensions {} and {} respectively'.format(train_y.shape,test_y.shape))
 
-# Run the model on 80% data
-#@vivek: please remove the hard coded values
-clf.fit(X[0:2727,], y1[0:2727])  
-
-# Predict on 20%
-#@vivek: please remove the hard coded values
-pred = clf.predict(X[2728:X.shape[0]])
-
-#@vivek: please remove the hard coded values
-cm = confusion_matrix(y1[2728:y1.shape[0]], pred)
-print('Confusion matrix \n'.format(cm))
-
-# Do a reverse confustion matrix
-#@vivek: please remove the hard coded values
-cm = confusion_matrix(pred,y1[2728:y1.shape[0]])
-print('Reverse Confusion matrix \n'.format(cm))
+def run_svm():
+    # Declare a list of all possible kernels in svm.SVC
+    kernels = ['linear', 'poly', 'rbf', 'sigmoid', 'precomputed']
+    for kernel in kernels:
+        print('Running {} kernel'.format(kernel))
+        clf = svm.SVC(kernel=kernel)
+        clf.fit(train_x,train_y)
+        pred = clf.predict(test_x)
+        cm = confusion_matrix(test_y, pred)
+        print('Confusion matrix for {} is \n {}'.format(kernel,cm))
+    
+def main():
+    run_svm()
+    
+if __name__ == '__main__':
+    main()
