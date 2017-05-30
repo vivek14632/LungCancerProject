84d4f091c53ba49fa37909b8d39ddd86a61acb88
vivek14632 <vksingh.iiitb@gmail.com>
2017-05-30 17:12:29 -0400

    summary

--- models/summary.py
+++ models/summary.py
@@ -1,3 +1,11 @@
 # authors: vivek
 # To know how many in the Y variable are 0 and how may are 1 to
 # determine the baseline for a random coin toss
+
+import numpy as np
+from data import * 
+from load_data import *
+X,Y = load_data()
+print('Number of 1 in Y ='+str(sum(Y)))
+print('Total number of samples='+str(Y.shape[0]))
+print('Percenttage of 1s ='+str(sum(Y)/float(Y.shape[0])))
