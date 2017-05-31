bff5187653176a37f145b8ee06e05fb7b077cdb6
ShankarMaruvada <ravishankarm@mail.usf.edu>
2017-05-31 11:24:20 -0400

    Command line version

--- models/GradientBoostingModel.py
+++ models/GradientBoostingModel.py
@@ -1,16 +1,41 @@
-import numpy as np
-from sklearn.ensemble import GradientBoostingClassifier
-from data import *
+from sklearn import svm
+from sklearn import neighbors
+from sklearn.metrics import confusion_matrix
 
-x_dir =X_MAT_PATH
-y_dir =Y_MAT_PATH
+from load_data import *
+from train_test_split import *
+from load_data import *
+from svmModel import *
+from timer import *
 
-X = np.load(x_dir)
-y = np.load(y_dir)
+def run_gradBoost():
+    X,Y = load_data()
 
-#@ vivek: please remove the hard coded values
-X_train, X_test = X[:2000], X[2000:]
-y_train, y_test = y[:2000], y[2000:]
+    # Create train and test data
+    train_x,test_x = get_train_test_data(X)
+    print('Train and test data for X matrix created with dimensions {} and {} respectively'.format(train_x.shape,test_x.shape))
 
-clf = GradientBoostingClassifier(n_estimators=100, learning_rate=1.0,max_depth=1, random_state=0).fit(X_train, y_train)
-clf.score(X_test, y_test)
+    train_y,test_y = get_train_test_data(Y)
+    print('Train and test data for Y matrix created with dimensions {} and {} respectively'.format(train_y.shape,test_y.shape))
+
+    # The fit method of the estimator expects a 1d array and not a column-vector (which is what test_y is now). Change the shape of test_y to (n_samples, )
+    train_y = reshape_label_matrix(train_y)
+    
+    # Get the start time
+    get_start_time()
+    
+    gb = GradientBoostingClassifier(n_estimators=100, learning_rate=1.0,max_depth=1, random_state=0).fit(train_x,train_y)
+    gb.score(train_x,train_y)
+    pred = gb.predict(test_x)
+    cm = confusion_matrix(test_y, pred)
+    print('Confusion matrix : \n {}'.format(cm))
+    
+    # Get the stop time
+    get_stop_time()
+    
+def main():
+    # Run knn model
+    run_knn()
+    
+if __name__ == '__main__':
+    main()
