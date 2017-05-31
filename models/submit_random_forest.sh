a8f4f3c328899698ad46476af8999b610a9b4cc4
Rhomi <rhomielijah@gmail.com>
2017-05-31 14:56:27 -0400

    Create submit_random_forest.sh

---
+++ models/submit_random_forest.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+#SBATCH --comment=r-test
+#SBATCH --ntasks=1
+#SBATCH --job-name=SVMmodel
+#SBATCH --output=output.%j.r-test
+#SBATCH --time=2:00:00
+#SBATCH --mem=20G
+
+#### SLURM 4 processor R test to run for 1 hour.
+
+module purge
+module add apps/python/3.5.1
+
+python random_forest.py
