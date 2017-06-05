86defc07c840d31cc555fd39c4293352364e368e
Rhomi <rhomielijah@gmail.com>
2017-06-05 13:44:14 -0400

    Update load_data.py
    The path to complete Y matrix added. Now the load_data method will return 3 numpy matrices -> X, Y without patient labels, and Y with Patient Labels in the exact same order.

--- models/load_data.py
+++ models/load_data.py
@@ -7,13 +7,13 @@ from data import*
 from train_test_split import *
 
 def load_data():
-    X = np.load(X_MAT_PATH)
-    Y = np.load(Y_MAT_PATH)
-
-    print("X matrix with shape {} has been loaded".format(X.shape))
-    print("Y matrix with shape {} has been loaded".format(Y.shape))
-
-    # Converting dtype to int32
-    X = X.astype(int)
-    Y = Y.astype(int)
-    return X,Y
+	X = np.load(X_MAT_PATH)
+	Y = np.load(Y_MAT_PATH)
+	Y_PATIENT = np.load(PATIENT_MAT_PATH)
+	print("X matrix with shape {} has been loaded".format(X.shape))
+	print("Y matrix with shape {} has been loaded".format(Y.shape))
+	print("Y_PATIENT matrix with shape {} has been loaded".format(Y_PATIENT.shape))
+	# Converting dtype to int32
+	X = X.astype(int)
+	Y = Y.astype(int)
+	return X,Y,Y_PATIENT
