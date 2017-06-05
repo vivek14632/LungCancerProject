207375a82d85c737f336d37803a025ed96dcae0a
Rhomi <rhomielijah@gmail.com>
2017-06-05 14:49:53 -0400

    Update random_forest.py
    Code altered to load and X and Y matrices ( Y without patient labels).

--- models/random_forest.py
+++ models/random_forest.py
@@ -9,7 +9,10 @@ from timer import *
 
 def run_random_forest():
 
-	X,Y = load_data()
+	data = load_data()
+	
+	X = data[0]
+	Y= data[1]
 
 	# Create train and test data
 	train_x,test_x = get_train_test_data(X)
