e89e554c6178ea12a95836fb1088361ff65f3d67
vivek14632 <vksingh.iiitb@gmail.com>
2017-06-06 12:26:51 -0400

    issue with or

--- models/data.py
+++ models/data.py
@@ -3,7 +3,7 @@ user= os.getlogin()
 if(user=='cis1024'):
 	X_MAT_PATH='/home/ravi/final_matrix.npy'
 	Y_MAT_PATH='/home/cis1024/sample_images_Y.npy'
-elif (user=='root'|user=='vivek4'):
+elif (user=='root'or user=='vivek4'):
 	#X_MAT_PATH='/work/v/vivek4/final_matrix.npy'
 	#Y_MAT_PATH='/work/v/vivek4/sample_images_Y.npy'
 	Y_MAT_PATH='/work/v/vivek4/stage1_clean_bak_Y_matrix/sample_images.npy'
