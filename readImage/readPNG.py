d2c58f0c6a876c0af8a8d87141174762d01ee293
Vivek Kumar Singh <vksingh.iiitb@gmail.com>
2017-06-20 17:08:31 -0400

    Create readPNG.py

---
+++ readImage/readPNG.py
@@ -0,0 +1,12 @@
+# https://stackoverflow.com/questions/31386096/importing-png-files-into-numpy
+
+# pip install scipy
+
+# install the numpy from following website: http://www.lfd.uci.edu/~gohlke/pythonlibs/#numpy
+
+import scipy
+import numpy
+from scipy import misc
+import glob
+image=misc.imread('C:/Users/vivek4/Desktop/top.png')
+image
