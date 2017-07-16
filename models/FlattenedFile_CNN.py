2bc083518d6d7992ba971199358dbbf41ea12b5e
Deepak Kakkattil George <rhomielijah@gmail.com>
2017-07-16 12:32:03 -0400

    Update FlattenedFile_CNN.py
    Added code for saving the final flattened X matrix.

--- models/FlattenedFile_CNN.py
+++ models/FlattenedFile_CNN.py
@@ -1,19 +1,22 @@
 import numpy as np
 import os
 
+from NumpyMatrix import *
+
 #data_dir = "C:/Users/maruv/Downloads/sample_images_clean/"
 
-def def_main_matrix(data_dir):
+def main_matrix(data_dir):
     #define total slices
     total_slices = 0
     
     #define the patients directory
     patients = os.listdir(data_dir)
     
-    #calculate complete slices for given set of patients
+    #calculate completex slices for given set of patients
     for patient in patients[:]:
-        mat = np.load(data_dir + patient)
+        mat = np.load(data_dir+'/'+patient)
         total_slices += len(mat)
+		#print(total_slices)
     
     #define new matrix
     new_mat = np.ndarray(total_slices*1, dtype = object)
@@ -22,13 +25,30 @@ def def_main_matrix(data_dir):
    
 
 def create_flat_mat(matrix,data_dir):
-    new_counter = 0
-    patients = os.listdir(data_dir)
-    for patient in patients[:]:
-        patient = np.load(data_dir + patient)
-        
-        #flatten matrix
-        for i in range(len(patient)):
-            matrix[new_counter] = patient[i].ravel()
-            new_counter += 1
-    return matrix
+	new_counter = 0
+	patients = os.listdir(data_dir)
+	for patient in patients[:]:
+		print('Processing ',patient)
+		patient = np.load(data_dir+'/'+patient)
+
+		#flatten matrix
+		for i in range(patient.shape[0]):
+			matrix[new_counter] = patient[i].ravel()
+			new_counter += 1
+			print('Counter ' + str(new_counter)+' '+str(len(matrix)))
+	return matrix
+
+def main():
+	mat = main_matrix(data_dir)
+	final_matrix = create_flat_mat(mat,data_dir)
+
+
+	filename = 'flat_sample_py3.npy'
+	file = os.path.join(r'C:/Users/maruv/Downloads/sample_images_clean/Final"', filename)
+
+	saveNumpy(final_matrix, file)
+	print("Numpy matrix has been saved as {}".format(file))
+	print("Shape of the numpy matrix is {}".format(final_matrix.shape))
+    
+if __name__ == '__main__':
+    main()
