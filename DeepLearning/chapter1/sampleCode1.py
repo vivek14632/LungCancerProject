cbe99046dc58c04408685426042c64025826e59b
Vivek Kumar Singh <vksingh.iiitb@gmail.com>
2017-08-26 13:10:17 -0400

    Update sampleCode1.py

--- DeepLearning/chapter1/sampleCode1.py
+++ DeepLearning/chapter1/sampleCode1.py
@@ -1,3 +1,7 @@
+# Author: Vivek Singh
+# Check this for more information : https://keras.io/getting-started/sequential-model-guide/
+# Purpose: Sample Keras code to build first Sequential model
+
 # import packages
 from keras.models import Dense
 from keras.models import Sequential
@@ -8,4 +12,6 @@ model=Sequential()
 # add first layer with RELU activation function
 model.add(Dense(32, input_dim=784))
 model.add(Activation('relu'))
+
+
 
